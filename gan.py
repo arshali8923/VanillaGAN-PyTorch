f61a84f70483f6a9334a2065b416a309848814c0
arshali8923 <arshali8923@gmail.com>
2026-09-02 22:21:32 +0530

    GAN.py

---
+++ gan.py
@@ -0,0 +1,124 @@
+import torchvision
+import os
+from torch.utils.data import DataLoader , Dataset
+from torchvision import transforms
+from PIL import Image
+
+#image load => transform => dataset out of all img
+
+class ImageProcessor:
+    def __init__(self , root_dir_path , transformations = None):
+        self.root_dir_path = root_dir_path
+        self.transformations = transformations
+
+        # list of path for all images
+        self.all_img_path =  [os.path.join(root_dir_path , img) for img in os.listdir(root_dir_path)]
+
+    def __len__(self):
+        return len(self.all_img_path)
+
+    def __getitem__(self , idx):
+        img_path = self.all_img_path[idx]
+        img = Image.open(img_path).convert("RGB")
+
+        if self.transformations:
+            img = self.transformations(img)
+
+        return img
+
+root_dir_path = r"C:\Users\arsha\Desktop\prime ai&ml\GAN\img_align_celeba\img_align_celeba"
+
+transformations = transforms.Compose([
+    transforms.CenterCrop(178),
+    transforms.Resize(64),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5 , 0.5 , 0.5) , (0.5 , 0.5 , 0.5))  #[-1,1]
+])
+
+dataset = ImageProcessor(root_dir_path , transformations)
+print(f"loaded {len(dataset)} images")
+
+dataloader = DataLoader(dataset , batch_size = 128 , shuffle = True)
+
+"""Generator Network"""
+
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+
+class Generator(nn.Module):
+    def __init__(self , z_dim = 100 , img_channel = 3):
+        super(Generator , self).__init__()
+
+        #fully connected (dense) layer
+
+        self.model = nn.Sequential(
+            nn.Linear(z_dim , 256),  #100 => 256
+            nn.ReLU(),
+
+            nn.Linear(256 , 512),
+            nn.ReLU(),
+
+            nn.Linear(512 , 1024),
+            nn.ReLU(),
+
+            nn.Linear(1024 , 64 * 64 * img_channel),
+            nn.Tanh()   #[-1,1]  #take coz we match the real img structured to fake images
+        )   
+
+    def forward(self , z):
+        img = self.model(z)
+        img = img.view(img.size(0) , 3 , 64 , 64)   #.size return batch size  , combined o/p convert to image dimension 
+        return img
+
+"""DESCRIMINATOR"""
+
+class Descriminator(nn.Module):
+    def __init__(self , img_channel = 3):
+        super(Descriminator , self ).__init__()
+
+        self.model = nn.Sequential(
+            nn.flatten(), #4D => 1D
+
+            nn.Linear(img_channel * 64 * 64 , 1024),
+            nn.LeakyReLU(0.2 , inplace = True),
+
+            nn.Linear(1024 , 512),
+            nn.LeakyReLU(0.2 , inplace = True),
+
+            nn.Linear(512 , 256),
+            nn.LeakyReLU(0.2 , inplace = True),
+
+            nn.Linear(256 , 1),
+            nn.Sigmoid()   #probability of being real/fake
+        )
+
+        def forward(self , img):
+            return self.model(img)
+
+
+GAN_loss = nn.BCELoss()
+
+generator = Generator()
+g_optimizer = optim.Adam(generator.parameters() , lr = 0.0002 , betas = (0.5 , 0.999))
+
+discriminator = Descriminator()
+d_optimizer = optim.Adam(discriminator.parameters() , lr = 0.0002 , betas = (0.5 , 0.999))
+
+"""Device"""
+
+import torch
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+
+elif torch.cuda.is_available():
+    device = torch.device("gpu")
+
+else:
+    device = torch.device("cpu")
+
+#shift to the device
+
+generator = generator.to(device)
+discriminator = discriminator.to(device)
